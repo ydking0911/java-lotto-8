843b201673cba6363f7b43a8ca608ff1eb5f66fb
ydking0911 <ydking0911@gmail.com>
2025-11-02 19:49:12 +0900

    refactor: Application.java 리팩토링 및 기존 파일 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.application.usecase.LottoPurchaseUseCase;
+import lotto.application.usecase.LottoResultUseCase;
+import lotto.domain.service.LottoProfitCalculator;
+import lotto.domain.service.LottoPurchaseValidator;
+import lotto.domain.service.LottoResultService;
+import lotto.domain.service.LottoTicketGenerator;
+import lotto.ui.LottoController;
+import lotto.ui.view.InputView;
+import lotto.ui.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoTicketGenerator generator = new LottoTicketGenerator();
+        LottoPurchaseValidator purchaseValidator = new LottoPurchaseValidator();
+        LottoProfitCalculator profitCalculator = new LottoProfitCalculator();
+        LottoResultService resultService = new LottoResultService();
+        LottoPurchaseUseCase purchaseUseCase = new LottoPurchaseUseCase(generator, purchaseValidator);
+        LottoResultUseCase resultUseCase = new LottoResultUseCase(profitCalculator, resultService);
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoController controller = new LottoController(inputView, outputView, purchaseUseCase, resultUseCase);
+        controller.run();
     }
 }
